b57fc0947b647ffcc013093805f18c1b52b96f5d
dokimiki <lmznd6yiew@gmail.com>
2025-12-07 00:21:53 +0900

    エラーを解消

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,25 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:css-modules/recommended", "eslint-config-postcss"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
     {
         ignores: [".next/**", "node_modules/**", "public/**", "next-env.d.ts"],
-        plugins: ["@eslint/css"],
-        parserOptions: {
-            ecmaVersion: 2020, // Or your target ECMAScript version
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 2020,
         },
-        env: {
-            node: true, // If PostCSS config files are Node.js modules
-        },
-        overrides: [
-            {
-                files: ["*.css", "*.pcss", "*.postcss"], // Target your PostCSS files
-                customSyntax: "@eslint/css", // Use the custom syntax for CSS
-                rules: {
-                    // Specific rules for PostCSS files, e.g., if you use nested rules
-                },
-            },
-        ],
+    },
+    {
+        files: ["*.css", "*.pcss", "*.postcss"],
     },
 ];
 
