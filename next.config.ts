4b233c3dc971d38cffe5f007052daf1baee47f14
dokimiki <lmznd6yiew@gmail.com>
2025-12-07 00:59:40 +0900

    cspにCloudflareも追加

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.gstatic.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.gstatic.com https://static.cloudflareinsights.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src 'self' blob: data: https://*.googleusercontent.com;
     font-src 'self' https://fonts.gstatic.com;
-    connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://chukyo-passpal.firebaseapp.com https://www.googleapis.com;
+    connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://chukyo-passpal.firebaseapp.com https://www.googleapis.com https://cloudflareinsights.com;
     frame-src 'self' https://*.firebaseapp.com https://chukyo-passpal.firebaseapp.com;
     frame-ancestors 'self' chrome-extension:;
     object-src 'none';
